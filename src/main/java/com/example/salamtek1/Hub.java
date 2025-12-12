e417784cdcea004fe1a15ec98f40d65ed198f045
nsur-suhaib <nasaraldeenmohamad@gmail.com>
2025-12-13 00:26:27 +0200

    Update Hub.java

--- src/main/java/com/example/salamtek1/Hub.java
+++ src/main/java/com/example/salamtek1/Hub.java
@@ -1,26 +1,25 @@
 package com.example.salamtek1;
-
 import java.util.ArrayList;
 import java.util.List;
 
 class Hub {
     private String hubId, cityName;
-    private Location location;
-    private ArrayList<String> officerIds = new ArrayList<>();
-    public Hub(String hubId, String cityName, Location location) {
+       private Location location;
+private ArrayList<String> officerIds = new ArrayList<>();
+  public Hub(String hubId, String cityName, Location location) {
         this.hubId = hubId;
-        this.cityName = cityName;
-        this.location = location;
+       this.cityName = cityName;
+       this.location = location;
     }
 
     public String getHubId() {
-        return hubId;
+ return hubId;
     }
-    public String getCityName() {
-        return cityName;
+public String getCityName() {
+    return cityName;
     }
-    public Location getLocation() {
-        return location;
+public Location getLocation() {
+    return location;
     }
     public void addOfficer(String officerId) {
         officerIds.add(officerId);
