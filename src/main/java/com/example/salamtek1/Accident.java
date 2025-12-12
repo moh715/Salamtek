13a1f293eb3f5c9cdaa8a4c7f3147ce8e1621492
nsur-suhaib <nasaraldeenmohamad@gmail.com>
2025-12-12 23:56:28 +0200

    Update Accident.java

--- src/main/java/com/example/salamtek1/Accident.java
+++ src/main/java/com/example/salamtek1/Accident.java
@@ -1,8 +1,8 @@
 package com.example.salamtek1;
-import com.example.salamtek1.Reportable;
+ import com.example.salamtek1.Reportable;
 import javax.xml.stream.Location;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+ import java.time.LocalDateTime;
+  import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.*;
 
@@ -35,22 +35,22 @@ class Accident implements Reportable {
     // Interface Implementation for generating report text
     @Override
     public String generateReportDetails() {
-      return "Accident ID: " + accidentId + "\n" +
-      "Date: " + getFormattedReportTime() + "\n" +
+return "Accident ID: " + accidentId + "\n" +
+        "Date: " + getFormattedReportTime() + "\n" +
       "Status: " + status + "\n" +
-      "Total Cost: " + totalCost + " EGP\n" +
-      "Fault Party: " + atFaultPartyId;
+       "Total Cost: " + totalCost + " EGP\n" +
+    "Fault Party: " + atFaultPartyId;
 }
 
        public String getAccidentId() { return accidentId; }
 public String getReporterNationalId() { return reporterNationalId; }
-    public String getOtherPartyNationalId() { return otherPartyNationalId; }
-    public String getReporterLicensePlate() { return reporterLicensePlate; }
-    public String getOtherPartyLicensePlate() { return otherPartyLicensePlate; }
+   public String getOtherPartyNationalId() { return otherPartyNationalId; }
+      public String getReporterLicensePlate() { return reporterLicensePlate; }
+     public String getOtherPartyLicensePlate() { return otherPartyLicensePlate; }
     public Location getAccidentLocation() { return accidentLocation; }
     public AccidentStatus getStatus() { return status; }
     public String getAssignedOfficerId() { return assignedOfficerId; }
-    public String getAtFaultPartyId() { return atFaultPartyId; }
+public String getAtFaultPartyId() { return atFaultPartyId; }
     // changed return type to arrayList
     public ArrayList<String> getDamagedParts() { return damagedParts; }
     public double getTotalCost() { return totalCost; }
@@ -60,7 +60,7 @@ public String getReporterNationalId() { return reporterNationalId; }
      public void setAssignedOfficerId(String officerId) { this.assignedOfficerId = officerId; }
      public void setAtFaultPartyId(String atFaultPartyId) { this.atFaultPartyId = atFaultPartyId; }
     public void setTotalCost(double totalCost) { this.totalCost = totalCost; }
-    public void setNotes(String notes) { this.notes = notes; }
+ public void setNotes(String notes) { this.notes = notes; }
     public void addDamagedPart(String partName) { damagedParts.add(partName); }
 
     public String getFormattedReportTime() {
