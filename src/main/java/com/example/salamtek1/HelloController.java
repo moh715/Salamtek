272dcdd8517995723f363dc36b93f730b600168a
nsur-suhaib <nasaraldeenmohamad@gmail.com>
2025-12-13 00:20:22 +0200

    Update HelloController.java

--- src/main/java/com/example/salamtek1/HelloController.java
+++ src/main/java/com/example/salamtek1/HelloController.java
@@ -1,14 +1,11 @@
 package com.example.salamtek1;
-
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+welcomeText.setText("Welcome to JavaFX Application!");
     }
 }
