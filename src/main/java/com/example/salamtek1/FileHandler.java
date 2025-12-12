6745ab76335c092b8e320d5c844ac633cfc7830b
nsur-suhaib <nasaraldeenmohamad@gmail.com>
2025-12-13 00:10:25 +0200

    Update FileHandler.java

--- src/main/java/com/example/salamtek1/FileHandler.java
+++ src/main/java/com/example/salamtek1/FileHandler.java
@@ -1,19 +1,19 @@
 package com.example.salamtek1;
-
 import java.io.FileWriter;
 import java.io.IOException;
 
+
 class FileHandler {
-    // Static method to save accident report to a .txt file
+    // saves accident report to a .txt file
     public static void saveReportToFile(Accident accident) {
         String filename = "Accident_Report_" + accident.getAccidentId() + ".txt";
         try (FileWriter writer = new FileWriter(filename)) {
-            writer.write("=== SALAMTEK SYSTEM REPORT ===\n");
-            writer.write(accident.generateReportDetails());
-            writer.write("\n==============================\n");
-            System.out.println(">> Report saved to file: " + filename);
+writer.write("=== SALAMTEK SYSTEM REPORT ===\n");
+writer.write(accident.generateReportDetails());
+ writer.write("\n==============================\n");
+System.out.println(">> Report saved to file: " + filename);
         } 
-            catch (IOException e) {
+   catch (IOException e) {
             System.out.println("Error saving file: " + e.getMessage());
         }
     }
