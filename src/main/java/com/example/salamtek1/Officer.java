d354affadc9c8e5ced1d2cf29d505fbff64aa265
nsur-suhaib <nasaraldeenmohamad@gmail.com>
2025-12-13 00:31:32 +0200

    Update Officer.java

--- src/main/java/com/example/salamtek1/Officer.java
+++ src/main/java/com/example/salamtek1/Officer.java
@@ -1,15 +1,13 @@
 package com.example.salamtek1;
-
 import com.example.salamtek1.Person;
-
 import java.util.ArrayList;
 
-// Officer extends Person (Inheritance)
+// officer extends Person (Inheritance)
 class Officer extends Person {
     private String officerId;
     private String hubId;
     private boolean isAvailable = true;
-    // Changed List to ArrayList
+    // Changed List to arraylist
     private ArrayList<String> assignedAccidentIds = new ArrayList<>();
 
     public Officer(String officerId, String name, String email, String password, String hubId) {
@@ -17,11 +15,9 @@ class Officer extends Person {
         this.officerId = officerId;
         this.hubId = hubId;
     }
-
-    @Override
+@Override
     public String getRole() { return "Police Officer"; } // Polymorphism implementation
-
-    public String getOfficerId() {
+public String getOfficerId() {
         return officerId;
     }
     public String getHubId() {
